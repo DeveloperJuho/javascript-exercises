730df21c15052ca8650b6dfcd6c23f923d244142
JuhoR <60245517+DeveloperJuho@users.noreply.github.com>
2023-11-16 15:17:30 +0200

    Sum and Add complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,35 @@
 const add = function() {
-	
+  const numbers = Array.from(arguments);
+
+  /*/ for(let i = 0; i <= numbers.length; i++){
+    console.log(numbers[i]);
+  } /*/
+
+  let addTotal = numbers.reduce((total, item) => {
+    return total + Number(item);
+  }, 0);
+  console.log(addTotal);
+  return addTotal;
 };
 
 const subtract = function() {
 	
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+
+  /*/ for(let i = 0; i <= numbers.length; i++){
+    console.log(numbers[i]);
+  } /*/
+
+  let addSum = numbers.reduce((total, item) => {
+    console.log(Number(item));
+
+    return Number(total) + Number(item);
+  }, 0);
+
+  console.log(addSum);
+  return addSum;
 };
 
 const multiply = function() {
@@ -31,3 +53,5 @@ module.exports = {
   power,
   factorial
 };
+
+sum([1,3,5]);
